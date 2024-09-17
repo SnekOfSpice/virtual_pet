1098035fae29cb3d0471813b434b56f98848d4d1
Fio <leukert.fio@gmail.com>
2024-09-17 16:27:08 +0200

    add spritesheet + multi-screen support

--- character.gd
+++ character.gd
@@ -18,8 +18,29 @@ enum STATE{
 
 func _ready():
 	pet_state = STATE.LOOKAROUND
-	sprite.play("look_around")
+	play("look_around")
 	timer.start()
+
+func set_flip(left:bool):
+	var suffix : String
+	if sprite.flip_h:
+		suffix = "left"
+	else:
+		suffix = "right"
+	var name_base = str(sprite.animation).trim_suffix(str("_", suffix))
+	sprite.flip_h = left
+	play(name_base)
+
+func reverse_flip():
+	set_flip(not sprite.flip_h)
+
+func play(anim_name:String):
+	var suffix : String
+	if sprite.flip_h:
+		suffix = "left"
+	else:
+		suffix = "right"
+	sprite.play(str(anim_name, "_", suffix))
 
 func _on_timer_timeout():
 	if pet_state == STATE.WALK:
@@ -30,19 +51,22 @@ func _on_timer_timeout():
 	match pet_state:
 		STATE.IDLE :
 			timer.set_wait_time(randi_range(10, 200))
-			sprite.play("idle")
+			play("idle")
 		STATE.LOOKAROUND :
-			timer.set_wait_time(randi_range(10, 200))
-			sprite.play("look_around")
+			timer.set_wait_time(randi_range(2, 4))
+			play("look_around")
 		STATE.WALK :
 			timer.set_wait_time(randi_range(5, 60))
-			sprite.play("walk")
+			play("walk")
 		STATE.SLEEP :
 			timer.set_wait_time(randi_range(300, 1000))
-			sprite.play("sleep")
+			play("sleep")
 	timer.start()
 
 func change_state():
 	pet_state = randi_range(0,3)
 	if pet_state == STATE.WALK:
 		walking.emit()
+
+func can_move_in_walk():
+	return sprite.frame > 1 and sprite.frame < 6
--- main.gd
+++ main.gd
@@ -1,16 +1,14 @@
 extends Node2D
 
-@onready var _MainWindow: Window = get_window()
-@onready var char_sprite: AnimatedSprite2D = $Character/AnimatedSprite2D
+@onready var _MainWindow = get_window()
+#@onready var char_sprite: AnimatedSprite2D = $Character/AnimatedSprite2D
+@onready var character: Node2D = $Character
 @onready var emitter: CPUParticles2D = $Character/CPUParticles2D
 
-var player_size: Vector2i = Vector2i(100,104)
+var player_size: Vector2i = Vector2i(130,134)
 #The offset between the mouse and the character
 var mouse_offset: Vector2 = Vector2.ZERO
 var selected: bool = false
-#This will be the position of the pet above the taskbar
-var taskbar_pos: int = (DisplayServer.screen_get_usable_rect().size.y - player_size.y)
-var screen_width: int = DisplayServer.screen_get_usable_rect().size.x
 #If true the character will move
 var is_walking: bool = false
 var walk_direction: int = 1
@@ -22,9 +20,26 @@ func _ready():
 	_MainWindow.min_size = player_size
 	_MainWindow.size = _MainWindow.min_size
 	#Places the character in the middle of the screen and on top of the taskbar
-	_MainWindow.position = Vector2i(DisplayServer.screen_get_size().x/2 - (player_size.x/2), taskbar_pos)
+	_MainWindow.position = Vector2i(DisplayServer.screen_get_size().x/2 - (player_size.x/2), get_pos_in_taskbar() + player_size.y * 0.5)
+
+func get_current_usable_screen_rect() -> Rect2:
+	#print(_MainWindow.position)
+	var mouse_pos = _MainWindow.position# DisplayServer.mouse_get_position()
+	var current_screen:int
+	var screen_rect:Rect2
+	for i in DisplayServer.get_screen_count():
+		var pos = DisplayServer.screen_get_position(i)
+		var size = DisplayServer.screen_get_size(i)
+		#printt(i, pos, size, mouse_pos)
+		if mouse_pos.x >= pos.x and mouse_pos.x <= pos.x + size.x:
+			if mouse_pos.y >= pos.y and mouse_pos.y <= pos.y + size.y:
+				current_screen = i
+				screen_rect = Rect2(pos, size)
+				break
+	return screen_rect
 
 func _process(delta):
+	get_current_usable_screen_rect()
 	if selected:
 		follow_mouse()
 	if is_walking:
@@ -34,11 +49,19 @@ func _process(delta):
 	if Input.is_action_just_pressed("pet"):
 		emitter.emitting = true
 
+func get_pos_in_taskbar():
+	return get_current_usable_screen_rect().position.y + get_current_usable_screen_rect().size.y - player_size.y + 5
+
 func follow_mouse():
 	#Follows mouse cursor but clamps it on the taskbar
 	_MainWindow.position = Vector2i(clamp_on_screen_width((get_global_mouse_position().x 
 		 + mouse_offset.x),
-		 player_size.x), taskbar_pos) 
+		 player_size.x), get_pos_in_taskbar()) 
+	#_MainWindow.position.x = get_global_mouse_position().x + mouse_offset.x
+	#_MainWindow.position.y = clamp_to_task_bar(get_global_mouse_position().x + mouse_offset.x)
+		##clamp_on_screen_width(
+			##(, player_size.x),
+			##taskbar_y) 
 
 func move_pet():
 	#On right click and hold it will follow the pet and when released
@@ -50,26 +73,49 @@ func move_pet():
 		selected = false
 
 func clamp_on_screen_width(pos, player_width):
-	return clampi(pos, 0, screen_width - player_width)
+	return clampi(pos, 0, multi_screen_extents().x - player_width)
+
+#func clamp_on_screen_width(x:float) -> int:
+	## clamps in furthest x directions wrt player size
+	## down to taskbar
+	#
+	#return clampi(x, 0, multi_screen_extents().x - player_size.x)
+	
+func clamp_to_task_bar(x:float):
+	var rect = get_current_usable_screen_rect()
+	return rect.position.y + rect.size.y
+
+func multi_screen_extents() -> Vector2:
+	var extents := Vector2.ZERO
+	for i in DisplayServer.get_screen_count():
+		var rect = DisplayServer.screen_get_usable_rect(i)
+		
+		extents.x = max(extents.x, rect.position.x + rect.size.x)
+		extents.y = max(extents.y, rect.position.y + rect.size.y)
+	return extents
 
 func walk(delta):
+	if not character.can_move_in_walk():
+		return
 	#Moves the pet
 	_MainWindow.position.x = _MainWindow.position.x + WALK_SPEED * delta * walk_direction
 	#Clamps the pet position on the width of screen
 	_MainWindow.position.x = clampi(_MainWindow.position.x, 0
 			,clamp_on_screen_width(_MainWindow.position.x, player_size.x))
+	_MainWindow.position.y = get_pos_in_taskbar()
 	#Changes direction if it hits the sides of the screen
-	if ((_MainWindow.position.x == (screen_width - player_size.x)) or (_MainWindow.position.x == 0)):
+	if ((_MainWindow.position.x == (multi_screen_extents().x - player_size.x)) or (_MainWindow.position.x == 0)):
 		walk_direction = walk_direction * -1
-		char_sprite.flip_h = !char_sprite.flip_h
+		character.reverse_flip()
 
 func choose_direction():
 	if (randi_range(1,2) == 1):
 		walk_direction = 1
-		char_sprite.flip_h = false
+		character.set_flip(false)
+		#char_sprite.flip_h = false
 	else:
 		walk_direction = -1
-		char_sprite.flip_h = true
+		character.set_flip(true)
 
 func _on_character_walking():
 	is_walking = true
